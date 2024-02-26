586b757d9f05d9b6bde7e074a95b376ea0109e21
crinux23 <oneacrina2003@gmail.com>
2024-02-26 09:20:18 +0200

    Initial commit. Added class main and ajutor

---
+++ Lab2/src/packg/ajutor.java
@@ -0,0 +1,13 @@
+package packg;
+
+public class ajutor {
+	void afisare() {
+		System.out.println("Hello world");
+	}
+	
+	void info() {
+		System.out.println("Nume proiect: Lab2");
+		System.out.println("Nume faculta: UPT");
+		System.out.println("Nume: Crina Onea");
+	}
+}
---
+++ Lab2/src/packg/main.java
@@ -0,0 +1,11 @@
+package packg;
+
+public class main {
+
+	public static void main(String[] args) {
+		ajutor a = new ajutor();
+		a.afisare();
+		a.info();
+	}
+
+}
