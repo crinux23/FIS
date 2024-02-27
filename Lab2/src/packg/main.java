a5c9cdc88037b53cc0ab0438dca9cbf7ff7d1d1d
crinux23 <oneacrina2003@gmail.com>
2024-02-27 20:19:11 +0200

    Reverse hunk

--- Lab2/src/packg/main.java
+++ Lab2/src/packg/main.java
@@ -1,21 +1,11 @@
 package packg;
 
-import java.util.Scanner;
-
 public class main {
-	
-	private static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		ajutor a = new ajutor();
-
-		String data= scan.nextLine();
-		
-		if(data == "Hello")
-			a.afisare();
-		else if(data =="info")
-			a.info();
-		else if (data == "exit")
-			return null;	
+		a.afisare();
+		a.info();
 	}
 
 }
